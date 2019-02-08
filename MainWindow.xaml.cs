a38c2bf89236ed0e89647d13bc0e6798af0a0165
Noah304555 <47399979+Noah304555@users.noreply.github.com>
2019-02-08 13:15:12 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿/*Noah Irving
+*Feb 6, 2019
+* Hello World, my first and fav program
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _304555HelloWorld
+{ 
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            Message.Content = "Bonjour, " + name;
+        }
+    }
+}
